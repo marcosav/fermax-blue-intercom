913de03451cf60d9d38e16ff243d22adcc8cf37a
marcosav <marcosav2010@gmail.com>
2024-03-07 20:55:06 +0100

    Improved token refresh & cached data

--- open_door.py
+++ open_door.py
@@ -10,48 +10,65 @@ import time
 
 from urllib.parse import quote
 
+from types import SimpleNamespace
+
+LOGGER = logging.getLogger("fermax_blue")
+
 CACHE_FILENAME = 'portal_cache.json'
-DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 cache_file_path = os.path.join(script_dir, CACHE_FILENAME)
 
 
-def update_cached_token(access_token: str, max_age: int):
-    logging.info('Caching token...')
+class OAuthTokenResponse():
+    access_token: str
+    token_type: str
+    refresh_token: str
+    expires_in: int
+    scope: str
+    jti: str
+    
+    
+class CachedTokenData():
+    def __init__(self, access_token: str, refresh_token: str, expires_at: datetime.datetime):
+        self.access_token = access_token
+        self.refresh_token = refresh_token
+        self.expires_at = expires_at
 
-    current_datetime = datetime.datetime.now().strftime(DATETIME_FORMAT)
 
-    cached_content = {
-        'access_token': access_token,
-        'max_age': max_age,
-        'updated_at': current_datetime,
+def update_cached_token(response: OAuthTokenResponse):
+    LOGGER.info('Caching token...')
+
+    now = datetime.datetime.utcnow()
+
+    cached_content: CachedTokenData = {
+        'access_token': response.access_token,
+        'refresh_token': response.refresh_token,
+        'expires_at': now + datetime.timedelta(seconds=response.expires_in),
     }
 
     with open(cache_file_path, 'w') as file:
-        json.dump(cached_content, file)
+        json.dump(cached_content, file, default=datetime_handler)
 
 
-def read_cached_token() -> Optional[str]:
+def read_cached_token() -> Optional[CachedTokenData]:
     try:
         with open(cache_file_path, 'r') as file:
             cached_content = json.load(file)
-
-            access_token = cached_content['access_token']
-            max_age = cached_content['max_age']
-            cache_datetime = datetime.datetime.strptime(
-                cached_content['updated_at'], DATETIME_FORMAT)
-
-        current_age = datetime.datetime.now() - cache_datetime
-        if current_age.total_seconds() >= max_age:
-            logging.info('Cached token has expired')
-            return None
-        else:
-            return access_token
+            cached_content['expires_at'] = datetime.datetime.fromisoformat(cached_content['expires_at'])
+            return CachedTokenData(**cached_content)
+        
     except FileNotFoundError:
-        logging.info('Cache file not found')
+        LOGGER.info('Cache file not found')
         return None
+    
+    except:
+        LOGGER.info('There was some error while reading cache file')
+        return None
+    
 
+def should_refresh(cached_token: CachedTokenData):
+    return datetime.datetime.utcnow() >= cached_token.expires_at
 
 # Fake client app and iOS device
 COMMON_HEADERS = {
@@ -73,7 +90,13 @@ AUTH_HEADERS = {
 AUTH_HEADERS.update(COMMON_HEADERS)
 
 
-def auth(cache: bool, username: str, password: str) -> str:
+def datetime_handler(obj):
+    if isinstance(obj, datetime.datetime): 
+        return obj.isoformat() 
+    raise TypeError("Type not serializable")
+    
+
+def auth(cache: bool, username: str, password: str) -> OAuthTokenResponse:
     username = quote(username)
     password = quote(password)
     auth_payload = f'grant_type=password&password={password}&username={username}'
@@ -81,17 +104,33 @@ def auth(cache: bool, username: str, password: str) -> str:
     response = requests.request(
         'POST', AUTH_URL, headers=AUTH_HEADERS, data=auth_payload)
 
-    parsed_json = response.json()
-    if 'error' in parsed_json:
-        raise RuntimeError(parsed_json['error_description'])
+    parsed_response = response.json()
+    if 'error' in parsed_response:
+        raise RuntimeError(parsed_response['error_description'])
 
-    access_token = parsed_json['access_token']
-    max_age = parsed_json['expires_in']
+    oauth_response = json.loads(response.text, object_hook=lambda d: SimpleNamespace(**d))
 
     if cache:
-        update_cached_token(access_token, max_age)
+        update_cached_token(oauth_response)
 
-    return access_token
+    return oauth_response
+
+
+def refresh_token(cache: bool, refresh_token: str) -> OAuthTokenResponse:
+    auth_payload = f'grant_type=refresh_token&refresh_token={refresh_token}'
+    response = requests.request(
+        'POST', AUTH_URL, headers=AUTH_HEADERS, data=auth_payload)
+    
+    parsed_response = response.json()
+    if 'error' in parsed_response:
+        raise RuntimeError(parsed_response['error_description'])
+    
+    oauth_response = json.loads(response.text, object_hook=lambda d: SimpleNamespace(**d))
+    
+    if cache:
+        update_cached_token(oauth_response)
+    
+    return oauth_response
 
 
 def get_json_headers(bearer_token: str) -> Dict[str, str]:
@@ -174,42 +213,51 @@ def main() -> None:
 
     # Program
 
-    access_token = None
+    oauth_token = None
 
-    if cache and not reauth:
-        access_token = read_cached_token()
+    if cache:
+        oauth_token = read_cached_token()
 
-    if not access_token:
-        logging.info('Logging in into Blue...')
+        
+    if oauth_token and (reauth or should_refresh(oauth_token)):
+        LOGGER.info('Refreshing Blue session...')
+        oauth_token = refresh_token(cache, oauth_token.refresh_token)
+    else:
+        LOGGER.info('Logging in into Blue...')
+        oauth_token = auth(cache, username, password)
 
-        access_token = auth(cache, username, password)
 
-    bearer_token = f'Bearer {access_token}'
+    bearer_token = f'Bearer {oauth_token.access_token}'
+
+
+    if reauth:
+        exit()
+
 
     if not provided_doors:
-        logging.info('Success, getting devices...')
+        LOGGER.info('Success, getting devices...')
 
         tag, device_id, access_ids = pairings(bearer_token)
 
-        logging.info(
+        LOGGER.info(
             f'Found {tag} with deviceId {device_id} ({len(access_ids)} doors), calling directed opendoor...')
 
     else:
-        logging.info(
+        LOGGER.info(
             f'Success, using provided deviceId {device_id}, calling directed opendoor...')
 
-    if not reauth:
-        # If user provided doors we open them all
-        if provided_doors:
-            for access_id in access_ids:
-                result = directed_opendoor(bearer_token, device_id, access_id)
-                logging.info(f'Result: {result}')
-                time.sleep(7)
 
-        # Otherwise we just open the first one (ZERO?)
-        else:
-            result = directed_opendoor(bearer_token, device_id, access_ids[0])
-            logging.info(f'Result: {result}')
+    # If user provided doors we open them all
+    if provided_doors:
+        for access_id in access_ids:
+            result = directed_opendoor(bearer_token, device_id, access_id)
+            LOGGER.info(f'Result: {result}')
+            time.sleep(7)
+
+    # Otherwise we just open the first one (ZERO?)
+    else:
+        result = directed_opendoor(bearer_token, device_id, access_ids[0])
+        LOGGER.info(f'Result: {result}')
 
 
 if __name__ == "__main__":
